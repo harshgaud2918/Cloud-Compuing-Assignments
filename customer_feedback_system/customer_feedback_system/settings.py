9b9bb62f23ed16b0428b27773cca71f6ac7fffaf
Mercury_80 <harsh.gaud18@gmail.com>
2021-09-23 03:13:06 +0530

    changed settings

--- customer_feedback_system/customer_feedback_system/settings.py
+++ customer_feedback_system/customer_feedback_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q)c!m^$gjr2r1qial8l@)st0l7-7-pbk!knc9x4j(cr2hy4q@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
