74c970a71e6bd911407ee564084f323afd931a34
Mercury_80 <harsh.gaud18@gmail.com>
2021-09-23 01:19:46 +0530

    Changed Db

--- customer_feedback_system/customer_feedback_system/settings.py
+++ customer_feedback_system/customer_feedback_system/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'customer_feedback_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'customer_feedback_database',
+        'HOST': 'mercurydbinstance.cuoyelbahfo0.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
+        'USER': 'mercury',
+        'PASSWORD': 'Mercury#rds18',
     }
 }
 
